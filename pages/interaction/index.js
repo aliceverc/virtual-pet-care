e97f5780b66bbd15f102db1eb0c121317ba3c460
Jan <jan.lahse@gmx.de>
2025-05-22 12:16:10 +0200

    created architecture for pet interaction page

---
+++ pages/interaction/index.js
@@ -0,0 +1,52 @@
+import styled from "styled-components";
+
+export default function InteractionPage() {
+  return (
+    <StyledSection>
+      <StyledHeader>Pet Name</StyledHeader>
+      <StyledImageSetion />
+      <StyledButtonContainer>
+        <StyledButton>Interact</StyledButton>
+        <StyledButton>Interact</StyledButton>
+        <StyledButton>Interact</StyledButton>
+      </StyledButtonContainer>
+    </StyledSection>
+  );
+}
+
+const StyledSection = styled.section`
+  display: grid;
+  place-items: center;
+`;
+
+const StyledHeader = styled.h2`
+  padding: 15px;
+  width: 200px;
+  text-align: center;
+  border-bottom: 3px solid #5885da;
+`;
+
+const StyledImageSetion = styled.section`
+  margin-top: 20px;
+  width: 150px;
+  height: 150px;
+  border: 2px solid #333;
+  border-radius: 3px;
+`;
+
+const StyledButtonContainer = styled.section`
+  margin-top: 100px;
+  display: flex;
+  justify-content: center;
+  gap: 20px;
+  width: 100%;
+`;
+
+const StyledButton = styled.button`
+  font-family: inherit;
+  font-size: 1rem;
+  background-color: white;
+  padding: 5px 20px;
+  border-radius: 2px;
+  border: 2px solid #333;
+`;
