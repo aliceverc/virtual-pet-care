c468886fb79e28823fd3ff11f4fd1725b7e8592f
Jan <jan.lahse@gmx.de>
2025-05-22 13:04:27 +0200

    added navigation for pet interaction and details pages

---
+++ components/PetNav.js
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+
+export default function PetNav() {
+  return (
+    <StyledSection>
+      <StyledButtonActive>Interaction</StyledButtonActive>
+      <StyledButtonInactive>Details</StyledButtonInactive>
+    </StyledSection>
+  );
+}
+
+const StyledSection = styled.section`
+  position: fixed;
+  width: 100%;
+  bottom: 0;
+  border-top: 3px solid #5885da;
+  display: flex;
+  justify-content: space-evenly;
+`;
+
+const StyledButtonInactive = styled.button`
+  margin: 15px;
+  font-family: inherit;
+  font-size: 1rem;
+  background-color: white;
+  padding: 5px 20px;
+  width: 160px;
+  border-radius: 3px;
+  border: 3px solid #aaa;
+`;
+
+const StyledButtonActive = styled(StyledButtonInactive)`
+  border: none;
+  background-color: #5885da;
+  color: white;
+`;
