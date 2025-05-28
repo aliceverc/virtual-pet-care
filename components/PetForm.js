15e77d391c1fbf6d2b1346507b3c8516709ef01c
Jan <jan.lahse@gmx.de>
2025-05-28 15:15:22 +0200

    created form for adding a pet

--- components/PetForm.js
+++ components/PetForm.js
@@ -1,30 +1,122 @@
+import { useState } from "react";
 import styled from "styled-components";
 
-export default function PetForm(onSubmit, onClose, onUpdatePreview) {
+export default function PetForm({ onSubmit, onClose, onUpdatePreview }) {
+  const [colorAmount, setColorAmount] = useState(1);
+
   return (
     <StyledForm onSubmit={onSubmit}>
       <h2>Create your Pet:</h2>
-      <SingleLineDiv>
+      <SingleLineCentered>
         <label htmlFor="name">Name: </label>
         <input id="name" name="name" required></input>
-      </SingleLineDiv>
+      </SingleLineCentered>
       <PreviewPlaceholder />
-      <button>Update Preview</button>
+      <button type="button">Update Preview</button>
       <h3>Appearance</h3>
-      <SingleLineDiv>
+      <SingleLine>
         <div>
-          <input type="radio" name="colorsAmount" id="singleColor" value="1" />
+          <input
+            type="radio"
+            name="colorsAmount"
+            id="singleColor"
+            value="1"
+            onChange={() => setColorAmount(1)}
+            defaultChecked
+          />
           <label htmlFor="singleColor">Single Color</label>
         </div>
         <div>
-          <input type="radio" name="colorsAmount" id="duoColor" value="2" />
+          <input
+            type="radio"
+            name="colorsAmount"
+            id="duoColor"
+            value="2"
+            onChange={() => setColorAmount(2)}
+          />
           <label htmlFor="duoColor">Duo Color</label>
         </div>
         <div>
-          <input type="radio" name="colorsAmount" id="tripleColor" value="3" />
+          <input
+            type="radio"
+            name="colorsAmount"
+            id="tripleColor"
+            value="3"
+            onChange={() => setColorAmount(3)}
+          />
           <label htmlFor="tripleColor">Triple Color</label>
         </div>
-      </SingleLineDiv>
+      </SingleLine>
+      <SingleLineCentered>
+        <input type="color" name="firstColor" defaultValue="#EA738D" />
+        {colorAmount > 1 && (
+          <input type="color" name="secondColor" defaultValue="#EA738D" />
+        )}
+        {colorAmount > 2 && (
+          <input type="color" name="thirdColor" defaultValue="#EA738D" />
+        )}
+      </SingleLineCentered>
+      <br />
+      <SingleLine>
+        <label htmlFor="width">Width:</label>
+        <input id="width" name="width" type="range" min="50" max="200" />
+      </SingleLine>
+      <SingleLine>
+        <label htmlFor="height">Height:</label>
+        <input id="height" name="height" type="range" min="50" max="200" />
+      </SingleLine>
+      <SingleLine>
+        <label htmlFor="shape">Shape:</label>
+        <input id="shape" name="shape" type="range" min="0" max="50" />
+      </SingleLine>
+      <SingleLine>
+        <label htmlFor="borderColor">Border Color:</label>
+        <input
+          id="borderColor"
+          name="borderColor"
+          type="color"
+          defaultValue="#EA738D"
+        />
+      </SingleLine>
+      <SingleLine>
+        <label htmlFor="borderStrength">Border Strength:</label>
+        <input
+          id="borderStrength"
+          name="borderStrength"
+          type="range"
+          min="0"
+          max="6"
+        />
+      </SingleLine>
+      <SingleLine>
+        <label htmlFor="borderStyle">Border Style:</label>
+        <select id="borderStyle" name="borderStyle">
+          <option value="solid">Solid</option>
+          <option value="dashed">Dashed</option>
+          <option value="dotted">Dotted</option>
+          <option value="double">Double</option>
+        </select>
+      </SingleLine>
+      <h3>Details</h3>
+      <SingleLine>
+        <label htmlFor="character">Character:</label>
+        <select id="character" name="character">
+          <option value="balanced">Balanced</option>
+          <option value="playful">Playful</option>
+          <option value="relaxed">Relaxed</option>
+          <option value="gourmet">Gourmet</option>
+        </select>
+      </SingleLine>
+      <StyledLabel htmlFor="description">Description:</StyledLabel>
+      <StyledTextArea
+        id="description"
+        rows="4"
+        maxLength="255"
+      ></StyledTextArea>
+      <SingleLineCentered>
+        <button onClick={onClose}>Cancel</button>
+        <button type="submit">Add Pet</button>
+      </SingleLineCentered>
     </StyledForm>
   );
 }
@@ -36,13 +128,28 @@ const StyledForm = styled.form`
   margin-bottom: 20px;
 `;
 
-const SingleLineDiv = styled.div`
+const SingleLine = styled.div`
+  width: 100%;
   display: flex;
-  gap: 10px;
+  justify-content: space-between;
+`;
+
+const SingleLineCentered = styled(SingleLine)`
+  justify-content: space-around;
 `;
 
 const PreviewPlaceholder = styled.div`
   width: 250px;
   height: 250px;
   border: 2px solid #333;
+  border-radius: 5px;
+`;
+
+const StyledLabel = styled.label`
+  place-self: start;
+`;
+
+const StyledTextArea = styled.textarea`
+  width: 100%;
+  border-radius: 5px;
 `;
