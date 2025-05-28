ea441b308dcf3c3d4c680e675603a4e72cc44428
Jan <jan.lahse@gmx.de>
2025-05-28 11:58:06 +0200

    fixed schema

--- db/models/Pet.js
+++ db/models/Pet.js
@@ -4,9 +4,26 @@ const { Schema } = mongoose;
 
 const petSchema = new Schema({
   id: { type: Number, required: true },
-  appearance: { type: Object },
-  details: { type: Object },
-  needs: { type: Object },
+  appearance: {
+    colors: [String, String, String],
+    height: Number,
+    width: Number,
+    shape: Number,
+    borderColor: String,
+    borderStrength: Number,
+    borderStyle: String,
+  },
+  details: {
+    name: String,
+    age: Number,
+    character: String,
+    description: String,
+  },
+  needs: {
+    hunger: Number,
+    energy: Number,
+    entertainment: Number,
+  },
 });
 
 const Pet = mongoose.models.Pet || mongoose.model("Pet", petSchema);
