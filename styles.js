f8afc82b841122286b576602d5a63ef1d7db7841
Felix <Fatkidkiter@gmail.com>
2025-06-17 16:30:44 +0200

    PetNav styling changed

--- styles.js
+++ styles.js
@@ -7,8 +7,31 @@ export default createGlobalStyle`
     box-sizing: border-box;
   }
 
-  body {
+  html, body {
     margin: 0;
+    padding: 0;
     font-family: system-ui;
+    min-height: 100vh;
+  }
+
+  body::before {
+    content: "";
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    background-image: url("/background2.png");
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+    opacity: 0.3;
+    z-index: -1;
+  }
+
+  body {
+    max-width: 600px;
+    margin: 0 auto;
+    padding: 0 16px;
   }
 `;
