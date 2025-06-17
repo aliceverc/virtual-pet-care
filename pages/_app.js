a317b18d0d5448612d291da388e4dffb762b47e4
Felix <Fatkidkiter@gmail.com>
2025-06-17 12:25:54 +0200

    background png added, opacity 0.3

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import { SWRConfig } from "swr";
 import GlobalStyle from "../styles";
-import "/styles/global.css";
+import styles from "styles";
 
 const fetcher = (url) => fetch(url).then((response) => response.json());
 
--- styles.js
+++ styles.js
@@ -11,4 +11,19 @@ export default createGlobalStyle`
     margin: 0;
     font-family: system-ui;
   }
+
+body::before {
+  content: "";
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 100vh;
+  width: 100vw;
+  background-image: url("/background2.png");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  opacity: 0.3;
+  z-index: -1;
+}
 `;
