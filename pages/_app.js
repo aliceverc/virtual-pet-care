30b7b35034370850ef86f06c65b9063fd4dd9343
Felix <Fatkidkiter@gmail.com>
2025-06-18 10:45:07 +0200

    fixed unnessecary nesting and layout

--- pages/_app.js
+++ pages/_app.js
@@ -8,19 +8,9 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <GlobalStyle />
-      <Layout>
         <SWRConfig value={{ fetcher }}>
           <Component {...pageProps} />
         </SWRConfig>
-      </Layout>
-    </>
+      </>
   );
 }
-
-const Layout = styled.div`
-  max-width: 600px;
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-`;
