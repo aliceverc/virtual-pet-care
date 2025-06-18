5452b2bc9a966cd2ed904ee294c9d165171b6bba
Jan <jan.lahse@gmx.de>
2025-06-18 12:24:07 +0200

    adjusted overlay to stop outside interactions

--- components/DeleteConfirmation.js
+++ components/DeleteConfirmation.js
@@ -20,8 +20,11 @@ const ConfirmationContainer = styled.section`
   position: fixed;
   left: 0;
   right: 0;
+  top: 0;
+  bottom: 0;
   display: flex;
   justify-content: center;
+  align-items: center;
   z-index: 99;
 `;
 
